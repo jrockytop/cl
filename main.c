6fc437f68582a5cf9951def8a73d4a11543c6b00
Jon Gettler <gettler@acm.org>
2012-05-18 16:26:35 -0500

    Add support for clisp.

--- main.c
+++ main.c
@@ -30,9 +30,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /* The common lisp magic for #! acceptance */
 char *shebang = "(set-dispatch-macro-character #\\# #\\!"
@@ -40,6 +43,65 @@ char *shebang = "(set-dispatch-macro-character #\\# #\\!"
 	        "  (declare (ignore c n))"
 	        "  (read-line s nil nil t)"
 	        "  nil))";
+
+/*
+ * Create a temporary file and fill it with the contents of a string.
+ */
+char *create_file(char *contents)
+{
+	char name[32];
+	FILE *f;
+
+	snprintf(name, sizeof(name), "/tmp/cl.%d.%d", getpid(), rand());
+
+	if ((f=fopen(name, "w")) == NULL) {
+		return NULL;
+	}
+
+	fprintf(f, "%s", contents);
+
+	fclose(f);
+
+	return strdup(name);
+}
+
+/*
+ * Run a command and return with its exit status when it is finished.
+ */
+int run_command(char *cmd, ...)
+{
+	va_list ap;
+	char *argv[64];
+	int i = 0;
+	pid_t child;
+	int status;
+
+	va_start(ap, cmd);
+
+	do {
+		argv[i++] = va_arg(ap, char*);
+	} while (argv[i-1] != NULL);
+
+	va_end(ap);
+
+	switch ((child=fork())) {
+	case -1:
+		break;
+	case 0:
+		execvp(cmd, argv);
+		break;
+	default:
+		while (1) {
+			waitpid(child, &status, 0);
+			if (WIFEXITED(status)) {
+				return WEXITSTATUS(status);
+			}
+		}
+		break;
+	}
+
+	return -1;
+}
 
 /*
  * Start up a lisp repl with rlwrap.   
@@ -62,6 +124,14 @@ void invoke_lisp_repl(char *lisp)
 	if (!strcmp("ecl", lisp)) {
 		execlp("rlwrap", "ecl", "ecl", "-q", "-eval", shebang,
 		       "-eval", "(setf *load-verbose* nil asdf:*asdf-verbose* nil)", NULL);
+	}
+
+	if (!strcmp("clisp", lisp)) {
+		char *file = create_file(shebang);
+		int rc;
+		rc = run_command("rlwrap", lisp, lisp, "-i", file, NULL);
+		unlink(file);
+		exit(rc);
 	}
 	
 	execlp("rlwrap", lisp, lisp, NULL);
@@ -99,6 +169,19 @@ void run_script(char *lisp, char *script, char *args)
 		       "-shell", script, NULL);
 	}
 	
+	if (!strcmp("clisp", lisp)) {
+		char *f_name = create_file(name);
+		char *f_args = create_file(args);
+		char *f_shebang = create_file(shebang);
+		int rc;
+		rc = run_command(lisp, lisp, "-i", f_name, "-i", f_args,
+				 "-i", f_shebang, "-q", "-q", script, NULL);
+		unlink(f_name);
+		unlink(f_args);
+		unlink(f_shebang);
+		exit(rc);
+	}
+
 	fprintf(stderr, "%s lisp not supported\n", lisp);
 	exit(-1);
 }
